d168bdf09a4149fbf034751e3dd3066596df4dcc
cb17225 <64376707+cb17225@users.noreply.github.com>
2025-05-01 17:28:56 -0700

    Import dataset
    - dataset imported

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
+import kagglehub
+
+path = kagglehub.dataset_download("maajdl/yeh-concret-data")
+print("Path to dataset files:", path)
 
 for i in range(10):
     print(f"Hello World_{random.randint(0,1000)}")
