4e8bff462d772cfd6cec142c1f8200df29bc0c0e
Set Paing <setpaing20022002@gmail.com>
2025-05-10 18:00:45 -0700

    wrapper removed

--- DNN_v2/tensorflow_skeleton/config.py
+++
@@ -1,7 +0,0 @@
-# training configuration
-EPOCHS = 50
-BATCH_SIZE = 32
-LEARNING_RATE = 0.001
-LOSS = 'mse'
-OPTIMIZER = 'adam'
-METRICS = ['mse']
--- DNN_v2/tensorflow_skeleton/utils.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
-def standardscalar_transform(X):
-    return StandardScaler().fit_transform(X)
-
-def minmaxscaler_transform(X):
-    return MinMaxScaler().fit_transform(X)
