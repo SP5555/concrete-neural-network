ffa4f7f63652a44157b6dd4b6a013d017e635ed0
jtthom133 <jtthom@ucdavis.edu>
2025-05-14 04:30:32 -0700

    MSE for degrees 1-5

--- poly_reg/poly_reg.py
+++ poly_reg/poly_reg.py
@@ -1,8 +1,10 @@
 import random
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 
@@ -46,6 +48,8 @@ def pol_reg(degree, X, y, fold_count, test_size=0.3, random_state=23):
 
     error_history = []
     models = []
+    splits = [] # keep track of how the data was split each fold so we can reuse it for MSE calculation later
+    fold_measures = []
 
     for train_index, test_index in kf.split(X_p):
         X_train, X_test = X_p[train_index], X_p[test_index]
@@ -60,16 +64,38 @@ def pol_reg(degree, X, y, fold_count, test_size=0.3, random_state=23):
         bias2 = np.mean(np.square(y_test - pred_avg))
         total_err = var + bias2
 
+        fold_measures.append((var, bias2, total_err))
+
         error_history.append(total_err)
         models.append(model)
+        splits.append((X_train, X_test, y_train, y_test))
 
     min_err_idx = np.argmin(error_history)
     best_model = models[min_err_idx]
-    return best_model
+    best_split = splits[min_err_idx]
+    return best_model, best_split, fold_measures
 
-dataset = pd.read_csv("data/Concrete_Data_Yeh.csv")
+dataset = pd.read_csv("../data/Concrete_Data_Yeh.csv")
 X = dataset.drop("csMPa", axis=1).values
 
 # assign values for y from dataset
 y = dataset["csMPa"].values
-best_model = pol_reg(5, X, y, 5)
+
+# degrees 1-5
+for degree in range(1,6):
+    # fold measures to measure variance and bias later, need to add still
+    best_model, best_split, fold_measures = pol_reg(degree, X, y, 5)
+
+    X_train, X_test, y_train, y_test = best_split
+
+    y_train_pred = best_model.predict(X_train)
+    y_test_pred = best_model.predict(X_test)
+
+    train_mse = mean_squared_error(y_train, y_train_pred)
+    test_mse = mean_squared_error(y_test, y_test_pred)
+
+    print("Degree:", degree, )
+    print("Train MSE:", train_mse)
+    print("Test MSE:", test_mse)
+    print("========")
+
